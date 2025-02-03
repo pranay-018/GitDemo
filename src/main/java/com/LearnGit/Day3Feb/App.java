6ee9c9147ec45f70d9c90c985c8ef9be857ab6f0
Gopidi Pranay <gopidi.pranay@mphasis.com>
2025-02-03 10:17:21 +0530

    DDeveloper2 Commit

--- src/main/java/com/LearnGit/Day3Feb/App.java
+++ src/main/java/com/LearnGit/Day3Feb/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("hello GitHub");     
+        System.out.println("hello GitHub");    
+	System.out.println("updated by the B1 branch"); 
     }
 }
